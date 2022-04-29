7daf90ad2a55f6a441a20a6c54b2f0ffd592638d
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-04-29 09:45:33 -0700

    Add emoji and langs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from os import getcwd
 from pathlib import Path
 from termcolor import colored
+import argparse
 import attr
 
 
@@ -18,6 +19,20 @@ PYTHON = Lang("Python", "🐍")
 RUST = Lang("Rust", "🦀")
 JAVA = Lang("Java")
 JAVASCRIPT = Lang("JS")
+NONE = Lang("NONE")
+
+
+def check_lang(path: Path) -> Lang:
+    if (path / Path("./setup.py")).exists():
+        return PYTHON
+
+    if (path / Path("./package.json")).exists():
+        return JAVASCRIPT
+
+    if (path / Path("./Cargo.toml")).exists():
+        return RUST
+
+    return NONE
 
 
 class Zebra:
@@ -39,8 +54,9 @@ ZEBRA = Zebra()
 
 
 class Directory:
-    def __init__(self, path: Path):
+    def __init__(self, path: Path, show_url: bool):
         self.path = path
+        self.show_url = show_url
 
     @property
     def has_git(self):
@@ -55,11 +71,19 @@ class Directory:
         return colored(str(self.path.name), "blue", attrs=["bold"])
 
     @property
+    def lang(self):
+        return check_lang(Path(self.path))
+
+    @property
+    def has_lang(self):
+        return self.lang != NONE
+
+    @property
     def url(self):
         if (config := (self.path / ".git" / "config")).exists():
             with open(config) as file:
                 for line in file:
-                    if line[:6] == '\turl =':
+                    if line[:6] == "\turl =":
                         return colored(line.split(" ")[-1].rstrip(), "green")
                 else:
                     return colored("local only", "yellow")
@@ -67,24 +91,31 @@ class Directory:
 
     def __repr__(self):
         first = self.git_status.ljust(5) + self.name
-        second = self.url
+        second = self.url if self.show_url else ""
 
         char = "-" if len(self.url) > 2 else " "
 
+        out = first
+
         if len(first) > 55:
-            return first + ZEBRA.pad(3, char=char) + second
+            out += ZEBRA.pad(3, char=char) + second
+        else:
+            length = 56 - len(first)
+            out += ZEBRA.pad(length + 2, char=char) + second
 
-        length = 56 - len(first)
-        return first + ZEBRA.pad(length + 2, char=char) + second
+        if self.has_lang:
+            out += self.lang.emoji + " " + self.lang.name
+
+        return out
 
 
-def create():
+def create(show_url: bool):
     dirs = []
     current_directory = Path(getcwd())
 
     for file_path in current_directory.iterdir():
         if file_path.is_dir():
-            directory = Directory(file_path)
+            directory = Directory(file_path, show_url)
             dirs.append(directory)
 
     return dirs
@@ -95,6 +126,14 @@ def run(dirs: list):
         print(directory)
 
 
+def parser():
+    parse = argparse.ArgumentParser()
+    parse.add_argument("-u", "--url", help="Show URL", action="store_true")
+    return parse.parse_args()
+
+
 if __name__ == "__main__":
-    directories = create()
+    args = parser()
+
+    directories = create(args.url)
     run(directories)
